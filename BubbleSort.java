6b61c21d021f91f42ec33e4c2091c618a9f99fcc
agordov <agordov96@gmail.com>
2017-03-10 01:33:28 +0300

    Added generics, fixed mistakes

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,45 +1,37 @@
-package bubble_sort;
-
+package bubbleSort;
+import java.lang.*;
 import java.util.Scanner;
 
-/**
- * Created by aleksey on 20.02.17.
- */
 public class BubbleSort {
-    public static int[] bubbleSorting(int[] arr) {
-        int i, j = 0;
-        int tmp;
-        boolean flag = true;
-        while (flag) {
-            flag = false;
-            j++;
-            for (i = 0; i < arr.length - j; i++) {
-                if (arr[i] < arr[i + 1]) {
-                    tmp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = tmp;
-                    flag = true;
+    private static < E extends Comparable<E>> void bubbleSort(E[] inputArray) {
+        int i, j;
+        E tmp;
+        for (i = 0; i < inputArray.length - 1; i++)
+            for (j = 0; j < inputArray.length - 1; j++)
+                if (inputArray[j].compareTo(inputArray[j + 1]) > 0) {
+                    tmp = inputArray[j];
+                    inputArray[j] = inputArray[j + 1];
+                    inputArray[j + 1] = tmp;
                 }
-            }
-        }
-        return arr;
+    }
+
+    private static <E> void printArray(E[] inputArray) {
+        for (E elem : inputArray)
+            System.out.print(elem + " ");
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] arr;
+        Integer[] arr;
         int i;
-        arr = new int[10];
-
+        arr = new Integer[10];
         Scanner scan = new Scanner(System.in);
         for (i = 0; i < 10; i++) {
             String str = scan.next();
             arr[i] = Integer.parseInt(str);
         }
-        arr = bubbleSorting(arr);
-
-        for (i = 0; i < 10; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
     }
 }
